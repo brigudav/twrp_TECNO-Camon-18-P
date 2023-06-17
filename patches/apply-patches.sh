e7cc8142c875752d9bbca51f3db9950813a1cb34
Alexander <alexvl1972@hotmail.com>
2023-06-17 23:29:02 +1000

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd bootable/recovery
+cd ./bootable/recovery
 patch -p1 < ../../device/Tecno/Camon18P/patches/0001-revert-save-settings-file-in-persist.patch
+patch -p1 < ./device/Tecno/Camon18P/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
